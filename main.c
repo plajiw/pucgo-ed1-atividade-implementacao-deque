eb7fd8f7f453fc5658baa08914a044af94dca591
plajiw <pabloribeiro.cont@gmail.com>
2026-03-15 20:25:24 -0300

    implementação na main

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "deque.h"
 
 int main()
@@ -6,13 +7,40 @@ int main()
     Deque deque;
     createDeque(&deque);
 
-    insertFront(&deque, 1);
-    insertFront(&deque, 2);
-    insertFront(&deque, 3);
+    printf("\nInserção 'head':\n");
+    insertFront(&deque, 10);
+    insertFront(&deque, 20);
+    insertFront(&deque, 30);
+    printFrontBack(&deque);
 
-    insertBack(&deque, 7);
-    insertBack(&deque, 8);
-    insertBack(&deque, 9);
+    printf("\nInserção 'tail':\n");
+    insertBack(&deque, 40);
+    insertBack(&deque, 50);
+    printFrontBack(&deque);
+
+    printf("\nVerificando balores:\n");
+    printf("Primeiro elemento: %d\n", firstElement(&deque));
+    printf("Último elemento: %d\n", lastElement(&deque));
+    printf("Tamanho atual: %d\n", size(&deque));
+
+    printf("\nBuscando valores:\n");
+    findElement(&deque, 20);
+    findElement(&deque, 99);
+
+    printf("\nRemovendo elementos:\n");
+    printf("Removendo da 'head' e da 'tail'\n");
+    popFront(&deque);
+    popBack(&deque);
+    printFrontBack(&deque);
+
+    printf("\nPrint inverso:\n");
+    printBackFront(&deque);
+
+    printf("\nLimpeza:\n");
+    makeEmptyDeque(&deque);
+    printf("Tamanho após limpeza: %d\n", size(&deque));
+    
+    destroyDeque(&deque);
 
     return 0;
 }
